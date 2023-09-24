b2065336042399b781ed626855b23ad9701df3de
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2023-09-24 16:03:03 +0200

    Initial commit

---
+++ TE22_2_Slagsmål/Program.cs
@@ -0,0 +1,17 @@
+﻿// Random generator = new Random();
+// int tal = generator.Next(20, 100);
+// Console.WriteLine(tal);
+
+int tal = 10;
+
+while (tal > 0)
+{
+  Console.WriteLine(tal);
+  tal--;
+}
+
+Console.WriteLine("FÄRDIG");
+
+
+
+Console.ReadLine();
